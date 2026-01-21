53bfbe3128a4e46f5597f4c983732cc6125e5a64
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-21 15:36:29 +0530

    to get response

---
+++ MuseumAPI/Model/LoginResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MuseumAPI.Model
+{
+    public class LoginResponseDto
+    {
+        public int? CustmId { get; set; }
+        public string? Custm_name { get; set; }
+        public string? Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
