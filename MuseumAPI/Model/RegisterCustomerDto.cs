de26537637307da4a56ca38a049055bc7835d5f1
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-21 15:37:04 +0530

    for register customer

---
+++ MuseumAPI/Model/RegisterCustomerDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MuseumAPI.Model
+{
+    public class RegisterCustomerDto
+    {
+        public string Custm_name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
