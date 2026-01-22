734f6fc0cce37f6f20b1544f1053d3dfccf2b5e6
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-22 17:52:06 +0530

    changes for google login

---
+++ MuseumAPI/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MuseumAPI.Model;
+
+namespace MuseumAPI.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<LoginResponseDto> LoginResponse { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LoginResponseDto>().HasNoKey();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
